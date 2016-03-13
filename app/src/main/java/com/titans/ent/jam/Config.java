9bbd806e05ae7ff8c814d1bac917ec84b866e12c
ImayaDismas <imayadismas@gmail.com>
2016-03-13 23:03:40 +0300

    Created a config activity

---
+++ app/src/main/java/com/titans/ent/jam/Config.java
@@ -0,0 +1,9 @@
+package com.titans.ent.jam;
+
+/**
+ * Created by imaya on 3/13/16.
+ */
+public class Config {
+    public static final String CLIENT_ID = "";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
