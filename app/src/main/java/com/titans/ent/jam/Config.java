f5d6b8aa5f8354906af3c70544d191436cc66898
ImayaDismas <imayadismas@gmail.com>
2016-03-14 00:17:51 +0300

    Added the client Id for soundloud

--- app/src/main/java/com/titans/ent/jam/Config.java
+++ app/src/main/java/com/titans/ent/jam/Config.java
@@ -4,6 +4,6 @@ package com.titans.ent.jam;
  * Created by imaya on 3/13/16.
  */
 public class Config {
-    public static final String CLIENT_ID = "";
+    public static final String CLIENT_ID = "f29719e082e7cb1e112f6b9cdb72e8fa";
     public static final String API_URL = "https://api.soundcloud.com";
 }
