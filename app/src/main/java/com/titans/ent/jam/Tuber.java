bcd3c3b538c4cff7649faa83be7742ab2bd91a5e
joseph muli <jayjaymuli00@gmail.com>
2016-03-14 11:45:08 +0300

    fixed layout bug, app done

--- app/src/main/java/com/titans/ent/jam/Tuber.java
+++ app/src/main/java/com/titans/ent/jam/Tuber.java
@@ -22,7 +22,7 @@ public class Tuber extends YouTubeBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.tuber);
 
         youTubePlayerView = (YouTubePlayerView) findViewById(R.id.tuber_view);
 
